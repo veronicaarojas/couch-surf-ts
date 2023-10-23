ddf08222a447bb9549f84acca0b2bff3df875a08
Veronica Rojas <ar_veronica@yahoo.com>
2023-10-23 16:08:11 +0100

    reviews

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("h1")
+const reviewTotalDisplay = document.querySelector('#reviews')
+
+const reviews = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+]
